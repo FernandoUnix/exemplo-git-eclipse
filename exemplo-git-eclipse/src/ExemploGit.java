fb92111add6b79b026b5bf9e5f2e240757b885b9
root-pc <root-pc@root>
2019-03-21 21:34:33 -0300

    commit final

--- exemplo-git-eclipse/src/ExemploGit.java
+++ exemplo-git-eclipse/src/ExemploGit.java
@@ -1,20 +1,7 @@
 
 public class ExemploGit {
 
-	/*
-	 * metodo main 
-	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.print("hello"); 
-=======
 		System.out.print("hello");
-		//alterado do hithub
-		//alterado do eclipse
-		//segundo commit repository
-		//terceiro commit repository
-		//commit elcipse
-		//terceiro commit repository
->>>>>>> branch 'master' of https://github.com/FernandoUnix/exemplo-git-eclipse.git
 	}
- }
+}
